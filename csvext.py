aeaa7545d39c1beec9c84d3289d8ea43f6289d09
kblee <kblee0@gmail.com>
2024-11-23 23:39:24 +0900

    init

--- csvext.py
+++ csvext.py
@@ -1,56 +1,59 @@
 import argparse
+import glob
+
 import pandas as pd
-import csv
-import sys
 
-def extract_columns_to_new_csv(args):
-    """
-    CSV 파일에서 특정 컬럼을 추출하여 새로운 CSV 파일을 생성합니다.
 
-    Args:
-        input_csv: 입력 CSV 파일의 경로
-        column_list_csv: 추출할 컬럼 목록이 담긴 CSV 파일의 경로
-        output_csv: 출력 CSV 파일의 경로
-    """
-
+def extract_columns_to_new_csv(param: object):
     # 컬럼 목록 CSV 파일 읽기
-    columns = pd.read_csv(args.column_list_csv, encoding=args.oenc, header=None).values.tolist()[0]
+    columns = pd.read_csv(param.columns, encoding=param.ienc, header=None).values.tolist()[0]
+    write_mode = 'w'
+    print_head = not param.noheader
+    oencoding = param.oenc
 
-    # 입력 CSV 파일 읽기
-    df = pd.read_csv(args.input_csv)
+    for filename in param.files:
+        # 입력 CSV 파일 읽기
+        df = pd.read_csv(filename, encoding=param.ienc, dtype=str)
+        df.fillna('', inplace=True)
 
-    # 지정된 컬럼 추출 및 병합
-    new_df = pd.DataFrame()
-    for column in columns:
-        if column in df.columns:
-            new_df[column] = df[column]
+        # 지정된 컬럼 추출 및 병합
+        new_df = pd.DataFrame()
+        for column in columns:
+            if column in df.columns:
+                new_df[column] = df[column]
 
-            for i in range(1,11):
-                rcol = column if i == 0 else column + "." + str(i)
-                if rcol in df.columns:
-                    new_df[column] = new_df[column].astype(str) + ',' + df[rcol].astype(str)
-                else:
-                    break
-        else:
-            new_df[column] = ""
+                for i in range(1,11):
+                    rcol = column if i == 0 else column + "." + str(i)
+                    if rcol in df.columns:
+                        new_df[column] = new_df[column].astype(str) + ',' + df[rcol].astype(str)
+                    else:
+                        if i > 1: new_df[column] = new_df[column].str.rstrip(',')
+                        break
+            else:
+                new_df[column] = ""
 
-    # 새로운 CSV 파일로 저장
-    new_df.to_csv(args.output_csv, encoding=args.oenc, index=False, header=(not args.noheader), quoting=csv.QUOTE_ALL)
+        # 새로운 CSV 파일로 저장
+        new_df.to_csv(param.output, mode=write_mode, encoding=param.oenc, index=False, header=print_head) #, quoting=csv.QUOTE_ALL)
+        write_mode = 'a'
+        print_head = False
+        if oencoding == 'utf-8-sig': oencoding = 'utf-8'
 
 if __name__ == "__main__":
     # ArgumentParser 객체 생성
-    parser = argparse.ArgumentParser(description='CSV Extractor.')
-    parser.add_argument('input_csv', help='Input CSV file name')
-    parser.add_argument('column_list_csv', help='column_list_csv CSV file name')
-    parser.add_argument('output_csv', help='output_csv CSV file name')
-    parser.add_argument('--ienc', help='Output file name', default='utf-8')
-    parser.add_argument('--oenc', help='Output file name', default='utf-8')
-    parser.add_argument('-n', '--noheader', help='Output file Header', action='store_true', default=False)
+    parser = argparse.ArgumentParser(prog='csvext', description='CSV Extractor.')
+    parser.add_argument('-c', '--columns', metavar='column_csv_filename', help='columns csv file name')
+    parser.add_argument('-o', '--output', metavar='output_filename', help='output csv file name')
+    parser.add_argument('--ienc', metavar='input_encoding', help='input encoding', default='utf-8')
+    parser.add_argument('--oenc', metavar='output_encoding', help='output encoding', default='utf-8-sig')
+    parser.add_argument('-n', '--noheader', help='no print csv header', action='store_true', default=False)
+    parser.add_argument('files', nargs='+', help='Input csv file name')
 
     args = parser.parse_args()
 
-    input_csv = sys.argv[1]
-    column_list_csv = sys.argv[2]
-    output_csv = sys.argv[3]
+    rfiles = []
+    for file in args.files:
+        rfiles.extend(glob.glob(file))
+
+    args.files = rfiles
 
     extract_columns_to_new_csv(args)
